4a4f0a97317251ceac8428e85032cb05d67ce25e
bAndelic <bruna.andelic@fer.hr>
2017-01-17 14:40:26 +0100

    front za studenta i pocetak profesora i login front

---
+++ eDnevnik/Api/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Api.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Predmeti()
+        {
+            return View();
+        }
+
+        public ActionResult Izostanci()
+        {
+            return View();
+        }
+    }
+}
