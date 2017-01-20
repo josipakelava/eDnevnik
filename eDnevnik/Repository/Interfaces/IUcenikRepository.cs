1343a7858dc9705540a7345ec1911622fa10b853
teapovic <teapovic@gmail.com>
2017-01-20 16:38:46 +0100

    UcenikRepository

---
+++ eDnevnik/Repository/Interfaces/IUcenikRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domena;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IUcenikRepository
+    {
+         Ucenik Find(int id);
+         IList<Izostanak> GetAllAbesnces(int id);
+         IList<Ocjena> GetAllGrades(int id);
+         IList<EvidencijaNastave> GetSchedule(int id);
+         IList<Biljeska> GetAllNotes(int id);
+
+    }
+}
---
+++ eDnevnik/Repository/Repositories/UcenikRepository.cs
@@ -0,0 +1,41 @@
+﻿using Domena;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UcenikRepository : IUcenikRepository
+    {
+        private ISession _session = DatabaseSessionFactory.OpenSession();
+
+        public Ucenik Find(int id)
+        {
+            return _session.QueryOver<Ucenik>().Where(u => u.idOsoba == id).List()[0];
+        }
+
+        public IList<Izostanak> GetAllAbesnces(int id)
+        {
+            return _session.QueryOver<Izostanak>().Where(u => u.ucenik.idOsoba == id).List();
+        }
+
+        public IList<Ocjena> GetAllGrades(int id)
+        {
+            return _session.QueryOver<Ocjena>().Where(u => u.ucenik.idOsoba == id).List();
+        }
+
+        public IList<Biljeska> GetAllNotes(int id)
+        {
+            return _session.QueryOver<Biljeska>().Where(u => u.ucenik.idOsoba == id).List();
+        }
+
+        public IList<EvidencijaNastave> GetSchedule(int id)
+        {
+            Ucenik ucenik = Find(id);
+            return _session.QueryOver<EvidencijaNastave>().Where(u => u.razred == ucenik.razred).List();
+        }
+    }
+}
