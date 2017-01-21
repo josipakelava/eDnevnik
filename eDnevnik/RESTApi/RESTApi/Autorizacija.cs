03b1438e90eae79e27ef5dfa20d75c36dab7dc0c
teapovic <teapovic@gmail.com>
2017-01-21 23:42:50 +0100

    REST autorizacija

---
+++ eDnevnik/RESTApi/RESTApi/Autorizacija.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RESTApi
+{
+    public class Autorizacija : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.Headers["Authorization"] != null && httpContext.Request.Headers["Authorization"].Split(' ').Length ==2)
+            {
+                var jwtToken = new JwtSecurityToken(httpContext.Request.Headers["Authorization"].Split(' ')[1]);
+                Claim[] claims = new Claim[5];
+                int i = 0;
+                foreach (Claim claim in jwtToken.Claims)
+                {
+                    switch (claim.Type)
+                    {
+                        case ClaimTypes.Sid:
+                        case ClaimTypes.Name:
+                        case ClaimTypes.Surname:
+                        case ClaimTypes.Email:
+                        case ClaimTypes.Role:
+                            claims[i++] = claim;
+                            break;
+                    }
+                }
+                Thread.CurrentPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims));
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ eDnevnik/RESTApi/RESTApi/Controllers/UcenikController.cs
@@ -0,0 +1,25 @@
+﻿using Repository;
+using Domena;
+using System;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Security.Claims;
+using System.Linq;
+
+namespace RESTApi.Controllers
+{
+
+    public class UcenikController : Controller
+    {
+        private IUcenikRepository _repository = new UcenikRepository();
+
+        [Autorizacija]
+        [HttpGet]
+        public String Get()
+        {
+            int id = Int32.Parse(((ClaimsPrincipal)Thread.CurrentPrincipal).Identities.ElementAt(0).Claims.ElementAt(0).Value);
+            return JsonConvert.SerializeObject(_repository.Find(id));
+        }
+    }
+}
