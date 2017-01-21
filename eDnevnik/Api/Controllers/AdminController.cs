6a5328a8705ae2ef6e016a0c20a0f5fe9b465e91
bAndelic <bruna.andelic@fer.hr>
2017-01-21 01:58:36 +0100

    Dodan administrator i popravljeno i uređeno sve za profesora

---
+++ eDnevnik/Api/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Api.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UrediOsobne()
+        {
+            return View();
+        }
+
+        public ActionResult UrediProfesor()
+        {
+            return View();
+        }
+    }
+}
