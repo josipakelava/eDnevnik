d6b00e5c7afef5ce50bbaff7fecb4341a2866663
teapovic <teapovic@gmail.com>
2017-01-25 20:10:56 +0100

    promjena naziva propertyja u jsonu

--- eDnevnik/Mapiranje/Domena/Kategorija.cs
+++ eDnevnik/Mapiranje/Domena/Kategorija.cs
@@ -1,8 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Domena
 {
     public class Kategorija
     {
+        [JsonProperty(PropertyName = "id")]
         public virtual int idKategorija { get; set; }
+        [JsonProperty(PropertyName = "name")]
         public virtual string naziv { get; set; }
     }
 
--- eDnevnik/Mapiranje/Domena/Mjesto.cs
+++ eDnevnik/Mapiranje/Domena/Mjesto.cs
@@ -1,9 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Domena
 {
     public class Mjesto
     {
-
+        [JsonProperty(PropertyName = "id")]
         public virtual int idMjesto { get; set; }
+        [JsonProperty(PropertyName = "name")]
         public virtual string ime { get; set; }
     }
 
